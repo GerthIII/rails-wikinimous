b391bead8d74f6519960ef28091229dd33a6e6e3
Glen Gerth <glengerth3@gmail.com>
2026-02-17 17:46:31 +0900

    App working. Need to style.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @articles = Article.find(params[:id])
+  end
+
+  def new
+    @articles = Article.new
+  end
+
+  def create
+    @articles = Article.new(article_params)
+    @articles.save
+
+    redirect_to articles_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @articles = Article.find(params[:id])
+    @articles.update(article_params)
+    
+    redirect_to article_path(@article)
+
+  end
+
+  def destroy
+    @articles = Article.find(params[:id])
+    @articles.destroy
+
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+end
